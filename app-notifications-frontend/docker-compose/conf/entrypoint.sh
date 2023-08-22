01ac0a0a5834c98b409f96d91a0b9d5f327b2432
Bruno Godoi <brunofgodoi@outloo.com.br>
2023-08-22 15:33:45 -0300

    first commit

---
+++ app-notifications-frontend/docker-compose/conf/entrypoint.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Up application frontend"
+nginx -g "daemon off;"
---
+++ app-notifications-ms/docker-compose/conf/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Run clear cache, route, config..."
+php artisan config:clear
+php artisan view:clear
+php artisan cache:clear
+php artisan route:cache
+php artisan config:cache
+php artisan optimize:clear
+
+echo "Run Migrations"
+php /var/www/artisan migrate
+
+echo "Run Seeders"
+php /var/www/artisan db:seed
+
+echo "Up application backend"
+php-fpm -D
+nginx -g "daemon off;"
