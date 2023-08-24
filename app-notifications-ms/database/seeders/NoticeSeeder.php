32a87d5add963dcd869eeaaebdaee72d29f9a161
Bruno Godoi <brunofgodoi@outloo.com.br>
2023-08-24 17:14:35 -0300

    Inclusão dependencia fakerphp/faker

--- app-notifications-ms/database/seeders/NoticeSeeder.php
+++ app-notifications-ms/database/seeders/NoticeSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Notice;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class NoticeSeeder extends Seeder
